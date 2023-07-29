f12cdc2ba16b0845da844321ad0c3fac9d0fa396
zwsp-200 <ayanxdev@gmail.com>
2023-07-29 21:44:27 +0600

    fix(ui): resize images, add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,40 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const name = "AyanW";
+const description = "Personal website and portfolio of AyanW";
+
 export default defineNuxtConfig({
     app: {
         head: {
+            title: name,
             charset: "utf8",
             viewport: "width=device-width, initial-scale=1",
-            title: "AyanW",
-            meta: [{ name: "description", content: "Student, Programmer" }],
+            htmlAttrs: { lang: "en" },
+            link: [
+                { rel: "icon", href: "/favicon.ico" },
+                { rel: "canonical", href: "https://ayanw.xyz" },
+                { rel: "alternate", href: "https://ayanw.xyz/" },
+            ],
+            meta: [
+                { name: "description", content: description },
+
+                { name: "author", content: "AyanW, ayanxdev@gmail.com" },
+                { name: "identifier-URL", content: "https://ayanw.xyz/" },
+                { name: "owner", content: "AyanW, ayanxdev@gmail.com" },
+                {
+                    name: "keywords",
+                    content: "ayanw, gamedev, programming, portfolio",
+                },
+                { property: "og:description", content: description },
+                {
+                    property: "og:image",
+                    content: "/images/meta/avatar.png",
+                },
+                { property: "og:locale", content: "en" },
+                { property: "og:title", content: name },
+                { property: "og:type", content: "website" },
+                { property: "og:url", content: "https://ayanw.xyz/" },
+                { name: "darkreader-lock" },
+            ],
         },
     },
     devtools: { enabled: true },
